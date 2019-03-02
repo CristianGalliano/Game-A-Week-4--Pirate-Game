17aebd47a100c78c44afe21acbf1e2708cf36018
Cristiwan <crisgalliano1998@gmail.com>
2019-03-02 18:24:54 +0000

    added 2dextras and healthbar

---
+++ GameAWeek4_Multiplayer/Assets/Scripts/healthBarScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthBarScript : MonoBehaviour
+{
+    public PlayerMovement player;
+    public Image healthbarImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scaleUI();
+    }
+
+    private void scaleUI()
+    {
+        healthbarImage.rectTransform.sizeDelta = new Vector2(player.health * 10, healthbarImage.rectTransform.sizeDelta.y);
+    }
+}
