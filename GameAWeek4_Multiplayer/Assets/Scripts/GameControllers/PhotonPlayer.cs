add7b9a9620b6c4f36a4a1c23efb23fd204dc6b8
PurplePhury3412 <purplephury3412@gmail.com>
2019-03-02 17:54:37 +0000

    random spawns around outside of islands

--- GameAWeek4_Multiplayer/Assets/Scripts/GameControllers/PhotonPlayer.cs
+++ GameAWeek4_Multiplayer/Assets/Scripts/GameControllers/PhotonPlayer.cs
@@ -8,15 +8,37 @@ public class PhotonPlayer : MonoBehaviour
 {
     private PhotonView PV;
     public GameObject myAvatar;
+    private bool validSpawnCheck = false;
+    public float spawnBoundLeft;
+    public float spawnBoundRight;
+    public float spawnBoundUp;
+    public float spawnBoundDown;
+    private float spawnPointX;
+    private float spawnPointY;
+    public float spawnZoneSize;
+    private int count = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         PV = GetComponent<PhotonView>();
-        int spawnpicker = Random.Range(0, GameSetup.GS.spawnpoints.Length);
+        while (!validSpawnCheck)
+        {
+            validSpawnCheck = false;
+            spawnPointX = (spawnBoundRight - spawnBoundLeft) * Random.value + spawnBoundLeft;
+            spawnPointY = (spawnBoundUp - spawnBoundDown) * Random.value + spawnBoundDown;
+            if (!((spawnPointX > (spawnBoundLeft + spawnZoneSize) && spawnPointX < (spawnBoundRight - spawnZoneSize))
+                && (spawnPointY > (spawnBoundDown + spawnZoneSize) && spawnPointY < (spawnBoundUp - spawnZoneSize))))
+            {
+                validSpawnCheck = true;
+            }
+            if (count > 100)
+                validSpawnCheck = true;
+            count++;
+        }
         if (PV.IsMine)
         {
-            myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerAvatar"), GameSetup.GS.spawnpoints[spawnpicker].position, GameSetup.GS.spawnpoints[spawnpicker].rotation, 0);
+            myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerAvatar"), new Vector3(spawnPointX, spawnPointY), Quaternion.identity, 0);
         }
     }
 
